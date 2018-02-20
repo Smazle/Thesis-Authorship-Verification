f22b55712fb8221d585206a271103cd4173dd0cb
Smazle <August.vinkel@gmail.com>
2018-02-20 13:21:25 +0100

    Finished initial draft

--- data_handling/format.py
+++ data_handling/format.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python3
 
-import sys
-import count
-import os
 import numpy as np
 import random
 
 SEED = 7
-NEWLINE = "$NL$"
-SEMI = "$SC$"
-PATH ="/home/smazle/Desktop/danske.csv"
+NEWLINE = '$NL$'
+SEMI = '$SC$'
+PATH = '/home/smazle/Desktop/danske.csv'
 MAX_LENGTH = 0
 
 random.seed(SEED)
@@ -17,25 +14,20 @@ random.seed(SEED)
 
 def Fix(text):
     # Replace escapes
-    data = list(map(lambda x: 
-        x.replace(NEWLINE, "\n").replace(SEMI, "\n"), text))
+    data = list(map(lambda x:
+                    x.replace(NEWLINE, '\n').replace(SEMI, '\n'), text))
     return np.array(data)
 
 
-student, text = np.loadtxt(PATH, delimiter=";", skiprows=1, dtype=str, unpack = True)
+student, text = np.loadtxt(
+    PATH, delimiter=';', skiprows=1, dtype=str, unpack=True)
 ids = list(map(str, range(0, len(student))))
 
 
 data = np.c_[ids, student, Fix(text)]
 data = np.array(sorted(data, key=lambda x: int(x[1])))
 
-
-
-
-
 MAX_LENGTH = max([len(x[-1]) for x in data])
-
-print(MAX_LENGTH)
 
 
 class Author:
@@ -48,33 +40,32 @@ class Author:
         # padding purposes
         self.oneHotMap = np.diag(np.ones(len(vocab) + 1))
 
-        self.encoded = np.array([[self.oneHotMap[vocab.index(x)] for x in y] for y in self.ordTexts])
+        self.encoded = np.array(
+            [[self.oneHotMap[vocab.index(x)] for x in y]
+                for y in self.ordTexts])
 
     def pad(self, max_length):
         temp = []
-        padValue = self.oneHotMap[-1,:]
+        padValue = self.oneHotMap[-1, :]
 
         for i, item in enumerate(self.encoded):
             item = list(item)
             diff = max_length - len(item)
 
             if diff != 0:
-                temp.append(list(np.concatenate((item, ([padValue] * diff))).shape))
+                temp.append(list(np.concatenate((item, ([padValue] * diff)))))
+            else:
+                temp.append(item)
 
-            self.encoded = np.array(temp)
-
-
+        self.encoded = np.array(temp)
 
     def randomText(self):
-        return random.choice(self.encoded) 
-
-
-
+        return random.choice(self.encoded)
 
 
 def GetProblems(authors, n):
-
-    problems = []
+    X = []
+    y = []
 
     for _ in range(0, n):
         keys = list(authors.keys())
@@ -84,40 +75,47 @@ def GetProblems(authors, n):
         author1 = authors[randomKey]
         author2 = authors[random.choice(keys)]
 
-        same_sample = np.concatenate((author1.randomText(), author1.randomText(), [1]))
-        different_sample = np.concatenate((author1.randomText(), author2.randomText(), [0]))
+        print(author1.randomText().shape)
+        print(author2.randomText().shape)
+        print(author2.encoded.shape)
+        print(author2.ordTexts.shape)
 
-        problems.append(same_sample)
-        problems.append(different_sample)
+        print()
 
-    return np.array(problems)
+        same_sample = np.concatenate(
+            (author1.randomText(), author1.randomText()))
+        different_sample = np.concatenate(
+            (author1.randomText(), author2.randomText()))
+
+        X.append(same_sample)
+        X.append(different_sample)
+        y.append(1)
+        y.append(0)
+
+    return np.array(X), np.array(y)
 
 
-
-
-
+def genVocabulary(data):
+    master_text = ''.join(data)
+    master_text = [ord(x) for x in master_text]
+    vocabulary = list(set(master_text))
+    return vocabulary
 
 
 authors = {}
 
-for i in range(3):
-    authors[i] = Author(data[:,-1])
+for x in data:
+    student = x[1]
+    if student in authors:
+        authors[student].texts.append(x[-1])
+    else:
+        authors[student] = Author([x[-1]])
 
+vocabulary = genVocabulary(data[:, -1])
 
-#for x in data:
-#    student = x[1]
-#    if student in authors:
-#        authors[student].texts.append(x[-1])
-#    else:
-#        authors[student] = Author([x[-1]])
-
-master_text = np.concatenate([[x[-1]] for x in data])
-master_text = "".join(master_text)
-master_text = [ord(x) for x in master_text]
-vocabulary = list(set(master_text))
-
-for _, author in authors.items():
+for q, author in authors.items():
     author.oneHotEncode(vocabulary)
     author.pad(MAX_LENGTH)
 
-temp = GetProblems(authors, 1)
+X, y = GetProblems(authors, 1)
+print(y)
