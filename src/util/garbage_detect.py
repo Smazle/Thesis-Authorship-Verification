45b156c3fc7a3da16b3bc41a08635c8ee0c1b4ab
Magnus Stavngaard <magnus@stavngaard.dk>
2018-03-06 10:19:22 +0100

    Add script to figure out if text contains garbage

---
+++ src/util/garbage_detect.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# -*- encoding: utf-8 -*-
+
+def is_garbage(text):
+    length = len(text)
+    spaces_length = len(list(filter(lambda x: x == ' ', text)))
+    ratio = spaces_length / length
+
+    return ratio < 0.05 or ratio > 0.95
+
+if __name__ == '__main__':
+    print(is_garbage("Hej med dig jeg hedder mor og jeg bor i et hus."))
