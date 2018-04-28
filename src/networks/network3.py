9c8c98d80e7b649d06838acbc1060b6abadd9400
Magnus Stavngaard <magnus@stavngaard.dk>
2018-04-28 20:42:18 +0200

    Refactor networks
    The part of the networks that are equal to a separate file.

--- src/networks/network3.py
+++ src/networks/network3.py
@@ -1,164 +1,69 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-from keras.layers import Dense, Convolution1D, GlobalMaxPooling1D, Input,\
-    Concatenate, Embedding, Dropout, merge
+import keras.layers as L
 from keras.models import Model
-from ..preprocessing import MacomReader
-import argparse
-from ..util import CSVWriter
-from keras.callbacks import ModelCheckpoint
-from keras.utils import plot_model
-import jsonpickle
 
 
-# Parse arguments.
-parser = argparse.ArgumentParser(
-    description='Simple NN for authorship verification'
-)
-parser.add_argument(
-    'datafile',
-    type=str,
-    help='Path to data file.'
-)
-parser.add_argument(
-    '--history',
-    type=str,
-    help='Path to file to write history to.',
-    default=None
-)
-parser.add_argument(
-    '--graph',
-    type=str,
-    help='Path to file to visualize network in.'
-)
-parser.add_argument(
-    '--reader',
-    type=str,
-    help='Use this pickled reader and not a new reader.'
-)
-parser.add_argument(
-    '--weights',
-    type=str,
-    help='Use the weights given as start weights instead of randomly' +
-         ' initializing.'
-)
-args = parser.parse_args()
+def model(reader):
+    inshape = (None, )
 
-# Either load reader from file or create a new one.
-if args.reader is not None:
-    with open(args.reader, mode='r') as reader_in:
-        reader = jsonpickle.decode(reader_in.read())
-else:
-    reader = MacomReader(
-        args.datafile,
-        batch_size=8,
-        vocabulary_frequency_cutoff=1 / 100000,
-        validation_split=0.95,
-        batch_normalization='pad',
-        pad=False
+    known_in = L.Input(shape=inshape, name='known_input')
+    unknown_in = L.Input(shape=inshape, name='unknown_input')
+
+    embedding = L.Embedding(len(reader.vocabulary_above_cutoff) + 2, 5,
+                            input_length=reader.max_len)
+
+    known_embed = embedding(known_in)
+    unknown_embed = embedding(unknown_in)
+
+    conv8 = L.Convolution1D(filters=500, kernel_size=8, strides=1,
+                            activation='relu', name='convolutional_8')
+
+    conv4 = L.Convolution1D(filters=500, kernel_size=4, strides=1,
+                            activation='relu', name='convolutional_4')
+
+    conv16 = L.Convolution1D(filters=200, kernel_size=8, strides=1,
+                             activation='relu', name='convolutional_8_2')
+
+    repr_known1 = L.GlobalMaxPooling1D(name='known_repr_8')(
+        conv8(known_embed))
+    repr_unknown1 = L.GlobalMaxPooling1D(name='unknown_repr_8')(
+        conv8(unknown_embed))
+
+    repr_known2 = L.GlobalMaxPooling1D(name='known_repr_4')(
+        conv4(known_embed))
+    repr_unknown2 = L.GlobalMaxPooling1D(name='unknown_repr_4')(
+        conv4(unknown_embed))
+
+    repr_known3 = L.GlobalMaxPooling1D(name='known_repr_8_2')(
+        conv16(known_embed))
+    repr_unknown3 = L.GlobalMaxPooling1D(name='unknown_repr_8_2')(
+        conv16(unknown_embed))
+
+    repr_known = L.Concatenate()([repr_known1, repr_known2, repr_known3])
+    repr_unknown = L.Concatenate()([repr_unknown1, repr_unknown2, repr_unknown3])
+
+    abs_diff = L.merge(
+        inputs=[repr_known, repr_unknown],
+        mode=lambda x: abs(x[0] - x[1]),
+        output_shape=lambda x: x[0],
+        name='absolute_difference'
     )
 
-    with open('reader.p', mode='w') as reader_out:
-        reader_out.write(jsonpickle.encode(reader))
+    dense1 = L.Dense(500, activation='relu')(abs_diff)
+    dense2 = L.Dense(500, activation='relu')(dense1)
+    dense3 = L.Dense(500, activation='relu')(dense2)
+    dense4 = L.Dense(500, activation='relu')(dense3)
 
-inshape = (None, )
+    pruned = L.Dropout(0.3)(dense4)
 
-known_in = Input(shape=inshape, name='known_input')
-unknown_in = Input(shape=inshape, name='unknown_input')
+    output = L.Dense(2, activation='softmax', name='output')(pruned)
 
-embedding = Embedding(len(reader.vocabulary_above_cutoff) + 2, 5,
-                      input_length=reader.max_len)
+    model = Model(inputs=[known_in, unknown_in], outputs=output)
 
-known_embed = embedding(known_in)
-unknown_embed = embedding(unknown_in)
+    model.compile(optimizer='adam',
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
 
-conv8 = Convolution1D(filters=500, kernel_size=8, strides=1,
-                      activation='relu', name='convolutional_8')
-
-conv4 = Convolution1D(filters=500, kernel_size=4, strides=1,
-                      activation='relu', name='convolutional_4')
-
-conv16 = Convolution1D(filters=200, kernel_size=8, strides=1,
-                       activation='relu', name='convolutional_8_2')
-
-repr_known1 = GlobalMaxPooling1D(name='known_repr_8')(
-    conv8(known_embed))
-repr_unknown1 = GlobalMaxPooling1D(name='unknown_repr_8')(
-    conv8(unknown_embed))
-
-repr_known2 = GlobalMaxPooling1D(name='known_repr_4')(
-    conv4(known_embed))
-repr_unknown2 = GlobalMaxPooling1D(name='unknown_repr_4')(
-    conv4(unknown_embed))
-
-repr_known3 = GlobalMaxPooling1D(name='known_repr_8_2')(
-    conv16(known_embed))
-repr_unknown3 = GlobalMaxPooling1D(name='unknown_repr_8_2')(
-    conv16(unknown_embed))
-
-repr_known = Concatenate()([repr_known1, repr_known2, repr_known3])
-repr_unknown = Concatenate()([repr_unknown1, repr_unknown2, repr_unknown3])
-
-abs_diff = merge(
-    inputs=[repr_known, repr_unknown],
-    mode=lambda x: abs(x[0] - x[1]),
-    output_shape=lambda x: x[0],
-    name='absolute_difference'
-)
-
-dense1 = Dense(500, activation='relu')(abs_diff)
-dense2 = Dense(500, activation='relu')(dense1)
-dense3 = Dense(500, activation='relu')(dense2)
-dense4 = Dense(500, activation='relu')(dense3)
-
-pruned = Dropout(0.3)(dense4)
-
-output = Dense(2, activation='softmax', name='output')(pruned)
-
-model = Model(inputs=[known_in, unknown_in], outputs=output)
-
-model.compile(optimizer='adam',
-              loss='categorical_crossentropy',
-              metrics=['accuracy'])
-
-steps_n = len(reader.training_problems) / reader.batch_size
-val_steps_n = len(reader.validation_problems) / reader.batch_size
-
-# Setup callbacks.
-callbacks = [
-    ModelCheckpoint(
-        'weights.{epoch:02d}-{val_loss:.2f}.hdf5',
-        monitor='val_loss',
-        save_best_only=False,
-        save_weights_only=True
-    )
-]
-
-if args.history is not None:
-    callbacks.append(
-        CSVWriter(
-            reader.generate_validation(), val_steps_n,
-            reader.generate_training(), steps_n, args.history,
-            args.weights is not None
-        )
-    )
-
-# If we are asked to visualize model, do so.
-if args.graph is not None:
-    plot_model(model, to_file=args.graph)
-
-# If we are given weights, load them.
-if args.weights is not None:
-    model.load_weights(args.weights)
-
-model.fit_generator(
-    generator=reader.generate_training(),
-    steps_per_epoch=steps_n,
-    epochs=1000,
-    validation_data=reader.generate_validation(),
-    validation_steps=val_steps_n,
-    callbacks=callbacks
-)
-
-model.save('final_model.hdf5')
+    return model
--- src/networks/r_network3.py
+++ src/networks/r_network3.py
@@ -1,134 +1,37 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import argparse
-import jsonpickle
-import keras
-import keras.backend as K
-import numpy as np
 from keras.models import Model
-from keras.layers.embeddings import Embedding
-from keras.layers import Dense, Concatenate, Input, Dropout, GRU, Reshape,\
-    Lambda, Flatten, Activation, merge, Convolution1D, MaxPooling1D, LSTM,\
-    Bidirectional, GlobalAveragePooling1D
-from keras.callbacks import ModelCheckpoint
-from keras.utils import plot_model
-from keras.layers.pooling import AveragePooling1D
-from ..preprocessing import MacomReader
-from ..util import CSVWriter
+import keras.layers as L
 
-# Parse arguments.
-parser = argparse.ArgumentParser(
-    description='Simple RNN for authorship verification'
-)
-parser.add_argument(
-    'datafile',
-    type=str,
-    help='Path to data file.'
-)
-parser.add_argument(
-    '--history',
-    type=str,
-    help='Path to file to write history to.',
-    default=None
-)
-parser.add_argument(
-    '--graph',
-    type=str,
-    help='Path to file to visualize network in.'
-)
-parser.add_argument(
-    '--reader',
-    type=str,
-    help='Use this pickled reader and not a new reader.'
-)
-parser.add_argument(
-    '--weights',
-    type=str,
-    help='Use the weights given as start weights instead of randomly' +
-         ' initializing.'
-)
-args = parser.parse_args()
 
-# Either load reader from file or create a new one.
-if args.reader is not None:
-    with open(args.reader, mode='r') as reader_in:
-        reader = jsonpickle.decode(reader_in.read())
-else:
-    reader = MacomReader(
-        args.datafile,
-        batch_size=1,
-        vocabulary_frequency_cutoff=1 / 100000,
-        validation_split=0.95
-    )
+def model(reader):
+    inshape = (reader.max_len, )
 
-    with open('reader.p', mode='w') as reader_out:
-        reader_out.write(jsonpickle.encode(reader))
+    known_in = L.Input(shape=inshape, dtype='int32')
+    unknown_in = L.Input(shape=inshape, dtype='int32')
 
-inshape = (reader.max_len, )
+    embedding = L.Embedding(len(reader.vocabulary_above_cutoff) + 2, 5)
 
-known_in = Input(shape=inshape, dtype='int32')
-unknown_in = Input(shape=inshape, dtype='int32')
+    known_emb = embedding(known_in)
+    unknown_emb = embedding(unknown_in)
 
-embedding = Embedding(len(reader.vocabulary_above_cutoff) + 2, 5)
+    lstm = L.Bidirectional(L.LSTM(100, return_sequences=True))
 
-known_emb = embedding(known_in)
-unknown_emb = embedding(unknown_in)
+    char_features_known = lstm(known_emb)
+    char_features_unknown = lstm(unknown_emb)
 
-lstm = Bidirectional(LSTM(100, return_sequences=True))
+    features_known = L.GlobalAveragePooling1D()(char_features_known)
+    features_unknown = L.GlobalAveragePooling1D()(char_features_unknown)
 
-char_features_known = lstm(known_emb)
-char_features_unknown = lstm(unknown_emb)
+    cos_distance = L.merge([features_known, features_unknown], mode='cos', dot_axes=1)
+    cos_distance = L.Reshape((1,))(cos_distance)
+    cos_similarity = L.Lambda(lambda x: 1 - x)(cos_distance)
 
-features_known = GlobalAveragePooling1D()(char_features_known)
-features_unknown = GlobalAveragePooling1D()(char_features_unknown)
+    model = Model(inputs=[known_in, unknown_in], outputs=cos_similarity)
 
-cos_distance = merge([features_known, features_unknown], mode='cos', dot_axes=1)
-cos_distance = Reshape((1,))(cos_distance)
-cos_similarity = Lambda(lambda x: 1 - x)(cos_distance)
+    model.compile(optimizer='adam',
+                  loss='binary_crossentropy',
+                  metrics=['accuracy'])
 
-model = Model(inputs=[known_in, unknown_in], outputs=cos_similarity)
-
-model.compile(optimizer='adam',
-              loss='binary_crossentropy',
-              metrics=['accuracy'])
-
-steps_n = len(reader.training_problems) / reader.batch_size
-val_steps_n = len(reader.validation_problems) / reader.batch_size
-
-# Setup callbacks.
-callbacks = [
-    ModelCheckpoint(
-        'weights.{epoch:02d}-{val_loss:.2f}.hdf5',
-        monitor='val_loss',
-        save_best_only=False,
-        save_weights_only=True
-    )
-]
-
-if args.history is not None:
-    callbacks.append(
-        CSVWriter(
-            reader.generate_validation(), val_steps_n,
-            reader.generate_training(), steps_n, args.history,
-            args.weights is not None
-        )
-    )
-
-# If we are asked to visualize model, do so.
-if args.graph is not None:
-    plot_model(model, to_file=args.graph, show_shapes=True)
-
-# If we are given weights, load them.
-if args.weights is not None:
-    model.load_weights(args.weights)
-
-model.fit_generator(
-    generator=reader.generate_training(),
-    steps_per_epoch=steps_n,
-    epochs=100,
-    validation_data=reader.generate_validation(),
-    validation_steps=val_steps_n
-)
-
-model.save('final_model.hdf5')
+    return model
