ec408e2fcd57356bd95ba519885d923749d75917
macomguest <macomguest@macom.dk>
2018-05-02 13:04:23 +0200

    Removed 4-Conv Layer

--- src/networks/network4.py
+++ src/networks/network4.py
@@ -22,59 +22,34 @@ def model(reader):
     conv8_1 = L.Convolution1D(filters=500, kernel_size=8, strides=1,
                               activation='relu', name='convolutional_8_1')
 
-    conv4_1 = L.Convolution1D(filters=500, kernel_size=4, strides=1,
-                              activation='relu', name='convolutional_4_1')
-
     max_1_1 = L.MaxPooling1D()
-    max_1_2 = L.MaxPooling1D()
 
-    conv4_1_known = max_1_1(conv4_1(known_embed))
-    conv8_1_known = max_1_2(conv8_1(known_embed))
-
-    conv4_1_unknown = max_1_1(conv4_1(unknown_embed))
-    conv8_1_unknown = max_1_2(conv8_1(unknown_embed))
+    conv8_1_known = max_1_1(conv8_1(known_embed))
+    conv8_1_unknown = max_1_1(conv8_1(unknown_embed))
 
     # Convolution 2
 
     conv8_2 = L.Convolution1D(filters=500, kernel_size=8, strides=1,
                               activation='relu', name='convolutional_8_2')
 
-    conv4_2 = L.Convolution1D(filters=500, kernel_size=4, strides=1,
-                              activation='relu', name='convolutional_4_2')
-
-    max_2_1 = L.MaxPooling1D()
     max_2_2 = L.MaxPooling1D()
 
-    conv4_2_known = max_2_1(conv4_2(conv4_1_known))
     conv8_2_known = max_2_2(conv8_2(conv8_1_known))
 
-    conv4_2_unknown = max_2_1(conv4_2(conv4_1_unknown))
     conv8_2_unknown = max_2_2(conv8_2(conv8_1_unknown))
 
     # Convolution 3
     conv8_3 = L.Convolution1D(filters=500, kernel_size=8, strides=1,
                               activation='relu', name='convolutional_8_3')
 
-    conv4_3 = L.Convolution1D(filters=500, kernel_size=4, strides=1,
-                              activation='relu', name='convolutional_4_3')
-
     # Glob Max Pool
-    repr_known1 = L.GlobalMaxPooling1D(name='known_repr_4')(
-        conv4_3(conv4_2_known))
-    repr_unknown1 = L.GlobalMaxPooling1D(name='unknown_repr_4')(
-        conv4_3(conv4_2_unknown))
-
     repr_known2 = L.GlobalMaxPooling1D(name='known_repr_8')(
         conv8_3(conv8_2_known))
     repr_unknown2 = L.GlobalMaxPooling1D(name='unknown_repr_8')(
         conv8_3(conv8_2_unknown))
 
-    repr_known = L.Concatenate()([repr_known1, repr_known2])
-    repr_unknown = L.Concatenate()(
-        [repr_unknown1, repr_unknown2])
-
     abs_diff = L.merge(
-        inputs=[repr_known, repr_unknown],
+        inputs=[repr_known2, repr_unknown2],
         mode=lambda x: abs(x[0] - x[1]),
         output_shape=lambda x: x[0],
         name='absolute_difference'
