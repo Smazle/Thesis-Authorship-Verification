8de6a63c07ee81a03ff8be4cad537c490babd42c
Magnus Stavngaard <magnus@stavngaard.dk>
2018-05-18 18:22:34 +0200

    Add test

--- src/test/preprocessing/macomreader_test.py
+++ src/test/preprocessing/macomreader_test.py
@@ -30,7 +30,20 @@ class TestMacomReader(unittest.TestCase):
                 encoded_line1 = reader.read_encoded_line(linereader, 2)
                 encoded_line2 = reader.read_encoded_line(linereader, 2)
 
-                self.assertEqual(encoded_line1, encoded_line2)
+                self.assertTrue(np.allclose(encoded_line1, encoded_line2))
+
+    # Reading with $NL$ should convert that to '\n'.
+    def test_read_encoded_line_3(self):
+        with FileOne() as f:
+            reader = MacomReader(
+                f.name, batch_size=1, pad=False, batch_normalization='pad',
+                min_len_characters=0, ignore_n_characters=0
+            )
+
+            with LineReader(f.name) as linereader:
+                encoded_line = reader.read_encoded_line(linereader, 4)
+
+                self.assertEqual(encoded_line[0][5], reader.channels[0].vocabulary_map['\n'])
 
 
 class FileOne:
@@ -41,6 +54,7 @@ class FileOne:
         self.f.write(b'author1;11-04-18;This is the first text\n')
         self.f.write(b'author1;11-04-17;This is the second text\n')
         self.f.write(b'author2;12-05-44;This is a text from another mother\n')
+        self.f.write(b'author2;11-11-94;This $NL$ text $NL$ contains $NAME$.\n')
         self.f.flush()
 
         self.name = self.f.name
