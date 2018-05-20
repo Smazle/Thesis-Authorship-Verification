21d8918b1b532eb9979d2580a757c966e456330a
Magnus Stavngaard <magnus@stavngaard.dk>
2018-05-20 17:38:45 +0200

    Add another test

--- src/test/preprocessing/macomreader_test.py
+++ src/test/preprocessing/macomreader_test.py
@@ -4,6 +4,7 @@
 import tempfile
 import unittest
 from src.preprocessing.macomreader import *
+import src.util.utilities as util
 
 
 class TestMacomReader(unittest.TestCase):
@@ -43,18 +44,47 @@ class TestMacomReader(unittest.TestCase):
             with LineReader(f.name) as linereader:
                 encoded_line = reader.read_encoded_line(linereader, 4)
 
-                self.assertEqual(encoded_line[0][5], reader.channels[0].vocabulary_map['\n'])
+                self.assertEqual(
+                    encoded_line[0][5], reader.channels[0].vocabulary_map['\n']
+                )
+
+    def test_generate_batch(self):
+        with FileOne() as f:
+            reader = MacomReader(
+                f.name, batch_size=2, pad=False, batch_normalization='pad',
+                min_len_characters=0, ignore_n_characters=0
+            )
+
+            problems = [(1, 2, 1), (2, 3, 0)]
+
+            with LineReader(f.name) as linereader:
+                knowns, unknowns, labels = reader.generate_batch(
+                    problems, linereader)
+                self.assertEqual(len(knowns), 1)  # We use one channel.
+                self.assertEqual(len(unknowns), 1)  # We use one channel.
+                self.assertEqual(len(labels), 2)  # We have a batch size of 2.
+
+                problem_1_len = max(f.text_lengths[1], f.text_lengths[2])
+                self.assertEqual(knowns[0].shape, (2, problem_1_len))
 
 
 class FileOne:
 
+    lines = [
+        b'ID;Date;Text\n',
+        b'author1;11-04-18;This is the first text\n',
+        b'author1;11-04-17;This is the second text\n',
+        b'author2;12-05-44;This is a text from another mother\n',
+        b'author2;11-11-94;This $NL$ text $NL$ contains $NAME$.\n',
+    ]
+
+    text_lengths = list(
+        map(lambda x: len(util.clean(x.decode('utf-8').split(';')[2])), lines))
+
     def __enter__(self):
         self.f = tempfile.NamedTemporaryFile().__enter__()
-        self.f.write(b'ID;Date;Text\n')
-        self.f.write(b'author1;11-04-18;This is the first text\n')
-        self.f.write(b'author1;11-04-17;This is the second text\n')
-        self.f.write(b'author2;12-05-44;This is a text from another mother\n')
-        self.f.write(b'author2;11-11-94;This $NL$ text $NL$ contains $NAME$.\n')
+        for line in self.lines:
+            self.f.write(line)
         self.f.flush()
 
         self.name = self.f.name
