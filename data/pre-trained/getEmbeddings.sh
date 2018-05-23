155ddc76d3a66f876cee3ecd19660fd412df6813
Smazle <August.vinkel@gmail.com>
2018-05-23 11:08:18 +0200

    Added pre-trained embeddings

---
+++ data/pre-trained/getEmbeddings.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+wget "https://s3-us-west-1.amazonaws.com/fasttext-vectors/wiki.da.vec"
+
---
+++ src/util/generate_emb_weight.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+
+def GetEmbeddingWeights(emb_path: str, reader):
+
+    weights = []
+    v_map = reader.channels[0].vocabulary_map
+
+    with open(emb_path, 'r') as f:
+        X, y = [int(x.strip()) for x in next(f).split(' ')]
+
+        weights = np.zeros(
+            (len(reader.channels[0].vocabulary_above_cutoff) + 2, y))
+
+        for idx, line in enumerate(f):
+            line = line.split(' ')
+            word = line[0]
+
+            w = [float(x) for x in line[1:-1]]
+
+            if word in v_map:
+                weights[v_map[word], :] = w
+
+    return weights
